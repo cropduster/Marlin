b7cf21452ca28d2f627f9ac31800a430fbeee914
cropduster <michael@kuebbeler.de>
2021-03-06 10:57:43 +0100

    steps updates

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2367,9 +2367,9 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       400        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       450        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  300  // (mA) RMS current for sensorless homing
-    #define X_MICROSTEPS     256    // 0..256
+    #define X_MICROSTEPS     32    // 0..256
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1        // -1..0: Not chained. 1: MCU MOSI connected. 2: Next in chain, ...
     //#define X_INTERPOLATE  true      // Enable to override 'INTERPOLATE' for the X axis
@@ -2385,9 +2385,9 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       400
+    #define Y_CURRENT       450
     #define Y_CURRENT_HOME 300
-    #define Y_MICROSTEPS     256
+    #define Y_MICROSTEPS     32
     #define Y_RSENSE          0.11
     #define Y_CHAIN_POS      -1
     //#define Y_INTERPOLATE  true
