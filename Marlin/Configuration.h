96153be54a1805cef369e8d3a377114e0c01a7b9
cropduster <michael@kuebbeler.de>
2021-03-29 14:20:38 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1048,7 +1048,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 64, 0, -2.75 }
+#define NOZZLE_TO_PROBE_OFFSET { 64.5, 0, -0.30 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1225,11 +1225,11 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 235
+#define X_BED_SIZE 230
 #define Y_BED_SIZE 215
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -22
+#define X_MIN_POS 0
 #define Y_MIN_POS -41
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
